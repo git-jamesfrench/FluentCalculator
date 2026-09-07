c2b49e3f8aa58a9aee2b77fd64e3b5a5514873fd
JamesFrench <119378901+git-jamesfrench@users.noreply.github.com>
2026-09-07 11:00:08 +0200

    Fixed an issue with prefix numbers

--- app/src/main/java/fr/jamesfrench/fluentcalculator/utils/IsValidOperator.kt
+++ app/src/main/java/fr/jamesfrench/fluentcalculator/utils/IsValidOperator.kt
@@ -8,22 +8,23 @@ fun String.isValidOperator(
     i: Int,
     isValid: Boolean = true,
     isReady: Boolean = true,
-    firstIteration: Boolean = true
+    iteration: Int = 0,
+    isFirstPrefix: Boolean = false
 ): Operation {
     val char = this.getOrElse(i) { T.Empty.value }
     // Check that the character before is a number
     val isReady =
-        if (firstIteration) this.getOrElse(i - 1) { T.Empty.value } in T.Number.values + T.CloseParentheses.value else isReady
+        if (iteration == 0) this.getOrElse(i - 1) { T.Empty.value } in T.Number.values + T.CloseParentheses.value else isReady
 
     return if (char in T.Number.values + T.OpenParentheses.value) { // A number, finish
-        Operation(isValid, isReady, i)
+        Operation(isValid, isReady || isFirstPrefix, i)
     } else if (i > this.lastIndex || char == T.CloseParentheses.value) { // End reached, invalid & finish
         Operation(isValid, false, i)
-    } else if (firstIteration && char in T.Operator.values) { // The first operator, continue
-        this.isValidOperator(i + 1, isValid, isReady, firstIteration = false)
-    } else if (char in T.Prefix.values && !firstIteration) { // A number prefix (- or +), continue
-        this.isValidOperator(i + 1, isValid, isReady, firstIteration = false)
+    } else if (iteration == 0 && char in T.Operator.values) { // The first operator, continue
+        this.isValidOperator(i + 1, isValid, isReady, iteration = 0 + 1, char in T.Prefix.values)
+    } else if (char in T.Prefix.values && iteration != 0) { // A number prefix (- or +), continue
+        this.isValidOperator(i + 1, isValid, isReady, iteration = iteration + 1, isFirstPrefix)
     } else { // Something else? Invalid but continue until number or end
-        this.isValidOperator(i + 1, false, isReady, firstIteration = false)
+        this.isValidOperator(i + 1, false, isReady, iteration = iteration + 1, isFirstPrefix)
     }
 }
