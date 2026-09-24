e5db717dcb4ef9518dae8bcd11d6d772d5d65491
JamesFrench <119378901+git-jamesfrench@users.noreply.github.com>
2026-09-24 23:37:52 +0200

    Added ObjectBox as a dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.objectbox)
+
 }
 
 android {
